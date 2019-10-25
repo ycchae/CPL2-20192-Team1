1748518758472a8094dc901c72b3df4f575c9706
CHAE-YC <yunchang.chae@gmail.com>
2019-10-26 02:35:42 +0900

    add alert

--- ionic/create_post/src/app/signup/signup.page.ts
+++ ionic/create_post/src/app/signup/signup.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-signup',
@@ -8,8 +9,13 @@ import { HttpHeaders, HttpClient } from '@angular/common/http';
   styleUrls: ['signup.page.scss'],
 })
 export class SignupPage{
+  
+  constructor(
+      private http: HttpClient,
+      private navCtrl: NavController,
+      private alertController : AlertController
+    ) {}
 
-  constructor(private http: HttpClient, private navCtrl: NavController) {}
   user = {
     email: '',
     password: '',
@@ -26,10 +32,33 @@ export class SignupPage{
     this.http.post(URL, form.value, {headers: headers})
     .subscribe(data=>{
       console.log(data['overlap_examine']);
+      this.alertController.create({
+        header: 'Confirm!',
+        subHeader: '계정 만들기 성공!',
+        message: '로그인 화면으로 이동합니다.',
+        buttons: [{
+          text: '확인',
+          handler:() =>{
+            this.navCtrl.navigateForward('/login');
+          }
+        }]
+      }).then(alert=>{
+        alert.present();
+      });
     }, error=>{
       console.log(error.status);
       console.log(error.error);
       console.log(error.headers);
+      this.alertController.create({
+        header: 'Reject!',
+        subHeader: '계정 만들기 실패ㅠ',
+        message: '잠시후 다시 시도해주세요.',
+        buttons: [{
+          text: '확인'
+        }]
+      }).then(alert=>{
+        alert.present();
+      });
     })
   }
 
@@ -40,5 +69,5 @@ export class SignupPage{
   //     header: 'Account Created',
   //     message: ''
   //   }).then(alert => alert.present());
-  // }
+  // // }
 }
